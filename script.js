fcd739b702fd941cf89527085e0f64b147d157be
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-20 11:02:58 +0530

    add js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var url="https://63770e1e81a568fc250af19a.mockapi.io/:user"
+var url="https://6379a4bd7eb4705cf282a5b9.mockapi.io/user"
 
 async function createData(){
+
     let data = {
-        "name": "vijayakumar",
-        "email": "vijayalingam@58gmailmail.com",
-        "phone": 97
+        name: "vijayakumar",
+        email: "vijayalingam@58gmail.com",
+        phone: 97887
     }
-    let res= await fetch(url,{
+    let res= await fetch (url, {
         method:"POST",
         body:JSON.stringify(data),
         headers:{
@@ -17,3 +18,39 @@ async function createData(){
     console.log(result); 
 }
 createData();
+
+async function readData(){
+    let res= await fetch(url);
+    var result1=await res.json();
+      console.log(result1)
+}
+readData();
+
+async function updateData(){
+
+    let data1 = {
+        name: "vijayakumar",
+        email: "vijayalingam@58gmail.com",
+        phone: 97887
+    }
+    let res1= await fetch (url+"/10", {
+        method:"PUT",
+        body:JSON.stringify(data1),
+        headers:{
+            "Content-type":"application/json;charset=UTF-8"
+        }
+    })
+    var result2 = await res1.json()
+    console.log(result2); 
+}
+updateData();
+
+async function deleteData(){
+
+    let res2= await fetch (url+"/16", {
+        method:"DELETE",
+    })
+    var result3 = await res2.json() 
+    console.log(result3); 
+}
+deleteData();
